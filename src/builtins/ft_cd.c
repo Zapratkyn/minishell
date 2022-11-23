c8113af04ca7368bc85f3619f7e0ad98d140ceda
lirockk <arthurdemurger26@gmail.com>
2022-11-23 16:21:14 +0100

    Little change

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,19 +6,26 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 13:40:09 by ademurge          #+#    #+#             */
-/*   Updated: 2022/11/23 15:01:35 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/11/23 16:20:25 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
 #include "../../inc/minish.h"
 
-char	*find_path(t_cmd *cmd)
+char	*find_path(t_mini *mini, t_cmd *cmd)
 {
 	char	*path;
+	char	*tmp;
 
 	path = NULL;
-	path = getcwd(path, 0);
+	if (!ft_strcmp("-", cmd->cmd[1]))
+		path = ft_strdup(mini_getenv(mini, "OLDPWD"));
+	else
+	{
+		path = getcwd(path, 0);
+		ft_strjoin(path, "/");
+		ft_strjoin(path, cmd->cmd[1]);
+	}
 	return (path);
 }
 
@@ -27,7 +34,7 @@ void	ft_cd(t_mini *mini, t_cmd *cmd)
 	char	*path;
 
 	update_pwd(mini, "OLDPWD");
-	path = find_path(cmd);
+	path = find_path(mini, cmd);
 	if (chdir(path) != -1)
 	{
 		free(path);
@@ -36,4 +43,3 @@ void	ft_cd(t_mini *mini, t_cmd *cmd)
 	update_pwd(mini, "PWD");
 	free(path);
 }
-*/
