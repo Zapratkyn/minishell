f0f91d8be59c166decd8c77f04c43e2a4a1913a7
lirockk <arthurdemurger26@gmail.com>
2022-12-02 14:04:54 +0100

    Norme

--- inc/minish.h
+++ inc/minish.h
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 11:22:21 by gponcele          #+#    #+#             */
-/*   Updated: 2022/12/02 13:56:36 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/12/02 14:04:40 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,117 +74,111 @@ int	g_status;
 ** Structures
 */
 
-typedef struct s_cmd		t_cmd;
-typedef struct s_heredoc	t_heredoc;
-typedef struct s_mini		t_mini;
-typedef struct s_var		t_var;
-
 /* Var structure */
-
-struct s_var
+typedef struct s_var
 {
-	char	*content;
-	t_var	*next;
-	t_var	*prev;
-};
+	char			*content;
+	struct s_var	*next;
+	struct s_var	*prev;
+}	t_var;
 
 /* Heredoc structure */
-struct s_heredoc
+typedef struct s_heredoc
 {
-	char		*content;
-	t_heredoc	*next;
-};
+	char				*content;
+	struct s_heredoc	*next;
+}	t_heredoc;
 
 /* Command structure */
-struct s_cmd
+typedef struct s_cmd
 {
-	char	**cmds;
-	char	*path;
-	int		infile;
-	int		outfile;
-	pid_t	pid;
-	int		fd[2];
-	t_cmd	*next;
-};
+	char			**cmds;
+	char			*path;
+	int				infile;
+	int				outfile;
+	pid_t			pid;
+	int				fd[2];
+	struct s_cmd	*next;
+}	t_cmd;
 
 /* Minishell structure */
-struct s_mini
+typedef struct s_mini
 {
 	int		g_status;
 	char	*prompt;
 	t_var	*var;
 	t_cmd	*cmd;
-};
+}	t_mini;
 
 /*
 ** Functions
 */
 
 /* Main */
-char						*get_prompt(t_mini *mini);
-t_mini						mini_init(char **env);
-int							mini_parser(t_mini *mini, char *str);
+char					*get_prompt(t_mini *mini);
+t_mini					mini_init(char **env);
+int						mini_parser(t_mini *mini, char *str);
 
 /* Utils.c */
-int							is_input(char *str);
-int							ft_quotes(char *str);
-void						mini_exit(t_mini *mini);
-char						*mini_getenv(t_mini *mini, char *var);
-void						mini_new_line(int sig);
-int							start_with_pipe(t_mini *mini, char *str, int i);
+int						is_input(char *str);
+int						ft_quotes(char *str);
+void					mini_exit(t_mini *mini);
+char					*mini_getenv(t_mini *mini, char *var);
+void					mini_new_line(int sig);
+int						start_with_pipe(t_mini *mini, char *str, int i);
 
 /* Parsing & Initialization */
-char						**clean_files(t_mini *mini, char **cmds,
-								int i, int j);
-t_cmd						*cmd_init(t_mini *mini);
-int							dol(char *str);
-char						**ft_split_cmd(t_mini *mini, char *s, int i,
-								int index);
-t_cmd						*get_cmd(t_mini *mini,
-								t_cmd *cmd, char *str, int i);
-char						*get_exec(t_mini *mini, t_cmd *cmd);
-void						get_infile(t_mini *mini, t_cmd *cmd, int i);
-int							get_infos_error(t_mini *mini, int i, char *s);
-void						get_outfile(t_mini *mini, t_cmd *cmd, int i, int j);
-void						get_path(t_mini *mini, t_cmd *cmd, int i);
-int							is_var(t_mini *mini, char *var, int j);
-void						mini_env(t_mini *mini);
-int							mini_heredoc(char *str, int i, int j);
-char						*to_empty(char *str);
+char					**clean_files(t_mini *mini, char **cmds,
+							int i, int j);
+t_cmd					*cmd_init(t_mini *mini);
+int						dol(char *str);
+char					**ft_split_cmd(t_mini *mini, char *s, int i,
+							int index);
+t_cmd					*get_cmd(t_mini *mini,
+							t_cmd *cmd, char *str, int i);
+char					*get_exec(t_mini *mini, t_cmd *cmd);
+void					get_infile(t_mini *mini, t_cmd *cmd, int i);
+int						get_infos_error(t_mini *mini, int i, char *s);
+void					get_outfile(t_mini *mini, t_cmd *cmd, int i, int j);
+void					get_path(t_mini *mini, t_cmd *cmd, int i);
+int						is_var(t_mini *mini, char *var, int j);
+void					mini_env(t_mini *mini);
+int						mini_heredoc(char *str, int i, int j);
+char					*to_empty(char *str);
 
 /* Execution */
-void						execute(t_mini *mini);
-void						exec_child(t_mini *mini, t_cmd *cmd);
+void					execute(t_mini *mini);
+void					exec_child(t_mini *mini, t_cmd *cmd);
 
 /* Execution UTILS */
-int							check_cmd(t_mini *mini, t_cmd *cmd);
-int							n_of_cmd(t_cmd *cmd);
-void						pipe_and_fork(t_mini *mini, t_cmd *cmd);
+int						check_cmd(t_mini *mini, t_cmd *cmd);
+int						n_of_cmd(t_cmd *cmd);
+void					pipe_and_fork(t_mini *mini, t_cmd *cmd);
 
 /* Builtin */
-int							ch_builtin(t_mini *mini, t_cmd *cmd);
-void						do_builtin(t_mini *mini, t_cmd *cmd);
-void						ft_cd(t_mini *mini, t_cmd *cmd);
-void						ft_echo(t_cmd *cmd);
-void						ft_env(t_mini *mini);
-void						ft_exit(t_mini *mini);
-void						ft_export(t_mini *mini, t_cmd *cmd);
-void						ft_pwd(t_mini *mini, t_cmd *cmd);
-void						ft_unset(t_mini *mini, t_cmd *cmd);
-int							par_builtin(t_mini *mini, t_cmd *cmd);
+int						ch_builtin(t_mini *mini, t_cmd *cmd);
+void					do_builtin(t_mini *mini, t_cmd *cmd);
+void					ft_cd(t_mini *mini, t_cmd *cmd);
+void					ft_echo(t_cmd *cmd);
+void					ft_env(t_mini *mini);
+void					ft_exit(t_mini *mini);
+void					ft_export(t_mini *mini, t_cmd *cmd);
+void					ft_pwd(t_mini *mini, t_cmd *cmd);
+void					ft_unset(t_mini *mini, t_cmd *cmd);
+int						par_builtin(t_mini *mini, t_cmd *cmd);
 
 /* Builtin UTILS */
-char						*ft_rev_strchr(t_mini *mini, char *str, char c);
-int							is_env(t_mini *mini, char *s);
-void						remove_quotes(t_cmd *cmd, int index, char *s);
+char					*ft_rev_strchr(t_mini *mini, char *str, char c);
+int						is_env(t_mini *mini, char *s);
+void					remove_quotes(t_cmd *cmd, int index, char *s);
 
 /* Error */
-void						ft_error(t_mini *mini, char *type, int is_exit);
+void					ft_error(t_mini *mini, char *type, int is_exit);
 
 /* Free */
-void						ft_free_cmd(t_cmd *cmd);
-void						ft_free_full_cmd(char **tab);
-void						ft_free_tab(char **tab, int len);
+void					ft_free_cmd(t_cmd *cmd);
+void					ft_free_full_cmd(char **tab);
+void					ft_free_tab(char **tab, int len);
 
 /* Linked List */
 int						ft_lst_index(t_var **l_var, t_var *var);
