59d83f3b7d3a233bc087eb43c6a3880ad9fd1591
lirockk <arthurdemurger26@gmail.com>
2022-12-06 17:25:01 +0100

    Change the exit message

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 16:55:54 by ademurge          #+#    #+#             */
-/*   Updated: 2022/12/06 16:38:54 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/12/06 17:24:03 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 void	ft_exit(t_mini *mini)
 {
+	int	is_cmd_exit;
+
+	is_cmd_exit = 0;
+	if (mini->cmd && mini->cmd->cmds && !ft_strcmp(mini->cmd->cmds[0], "exit"))
+		is_cmd_exit = 1;
 	ft_free_all(mini);
-	write(STDERR_FILENO, "\nexit\n", 6);
+	if (is_cmd_exit)
+		write(STDERR_FILENO, "exit\n", 6);
+	else
+		write(STDERR_FILENO, "\nexit\n", 6);
 	exit(g_status);
 }
