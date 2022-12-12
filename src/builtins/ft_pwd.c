fad485559e3556a72ee8806174e89f488c1c0661
lirockk <arthurdemurger26@gmail.com>
2022-12-12 11:35:45 +0100

    g_status on pwd

--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 10:52:09 by ademurge          #+#    #+#             */
-/*   Updated: 2022/12/12 10:51:52 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/12/12 11:35:27 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	ft_pwd(t_mini *mini, t_cmd *cmd)
 	char	*pwd;
 
 	if (cmd->cmds[1][0] == '-' && cmd->cmds[1][1])
+	{
+		g_status = 1;
 		return (ft_error(mini, "minishell: pwd: invalid option", NO_EXIT));
+	}
 	pwd = NULL;
 	pwd = getcwd(pwd, 0);
 	if (!pwd)
