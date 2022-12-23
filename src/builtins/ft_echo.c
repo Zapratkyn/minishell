a5c628a9242fd69f9fd0d6d4061e9a1968692a5c
lirockk <98538729+lirockk@users.noreply.github.com>
2022-12-23 22:01:32 +0100

    better echo

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gponcele <gponcele@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 13:39:51 by ademurge          #+#    #+#             */
-/*   Updated: 2022/12/22 16:42:06 by gponcele         ###   ########.fr       */
+/*   Updated: 2022/12/23 21:41:41 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,21 +36,33 @@ static int	*check_newl(t_mini *mini, t_cmd *cmd)
 	return (ret);
 }
 
+void	print_space(char **cmds, int i)
+{
+	if (cmds[i + 1] && cmds[i + 1][0])
+		write (STDOUT_FILENO, " ", 1);
+	else
+	{
+		i++;
+		while (cmds[i] && !cmds[i][0])
+			i++;
+		if (cmds[i] && cmds[i][0])
+			write (STDOUT_FILENO, " ", 1);
+	}
+}
+
 void	ft_echo(t_mini *mini, t_cmd *cmd)
 {
 	int	i;
 	int	*newl;
 
 	newl = check_newl(mini, cmd);
-	i = newl[0];
-	while (cmd->cmds[i])
+	i = newl[0] - 1;
+	while (cmd->cmds[++i])
 	{
-		if (i - 1 >= 0 && !cmd->cmds[i - 1][0])
-			write (STDOUT_FILENO, " ", 1);
+		if (!cmd->cmds[i][0])
+			continue ;
 		ft_putstr_fd(cmd->cmds[i], STDOUT_FILENO);
-		if (cmd->cmds[i][0] && cmd->cmds[i + 1] && cmd->cmds[i + 1][0])
-			write (STDOUT_FILENO, " ", 1);
-		i++;
+		print_space(cmd->cmds, i);
 	}
 	if (newl[1] == NEW_L)
 		write (STDOUT_FILENO, "\n", 1);
